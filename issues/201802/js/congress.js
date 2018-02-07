b353222b18730217f8e2d1deabd19fc999d30069
Yudivian Almeida <yudivian@gmail.com>
2018-02-07 15:47:46 -0500

    Adjusting scroll offset

--- issues/201802/js/congress.js
+++ issues/201802/js/congress.js
@@ -840,7 +840,7 @@ function init() {
         text: '#scroll_text1',
         step: '#scroll_text1 .step1',
         debug: false,
-        offset: 0.35
+        offset: 0.6
     })
         .onStepEnter(handleStepEnter1)
         .onStepExit(handleStepExit1)
@@ -853,7 +853,7 @@ function init() {
         text: '#scroll_text2',
         step: '#scroll_text2 .step2',
         debug: false,
-        offset: 0.35
+        offset: 0.6
     })
         .onStepEnter(handleStepEnter2)
         .onContainerEnter(handleContainerEnter2)
@@ -865,7 +865,7 @@ function init() {
         text: '#scroll_text3',
         step: '#scroll_text3 .step3',
         debug: false,
-        offset: 0.35
+        offset: 0.6
     })
         .onStepEnter(handleStepEnter3)
         .onStepExit(handleStepExit3)
